3994309a5ccf104e304f3c6a95eef766888d68c7
fffonion <fffonion@gmail.com>
2013-09-10 10:39:00 +0800

    fix cproxy crawler

--- cproxy_crawler.py
+++ cproxy_crawler.py
@@ -8,30 +8,33 @@ ip = '%s.%s.%s.%s' % (rrange(0, 255), rrange(0, 255), rrange(0, 255), rrange(0, 
 headers = {'Connection': 'keep-alive','Content-Type': 'application/x-www-form-urlencoded','X-Forward-For':ip,'Client_IP':ip}
 hdstart={'Expect':'100-continue'}
 hdtarg={'User-Agent':'Python-urllib/1.17'}
+cproxyver='2.3.5'
 hdstart.update(headers)
 hdtarg.update(headers)
-start='http://mycdb.sinaapp.com/index.php/Rest/Connect?%s'%rrange(100,999)
-targ='http://mycdb.sinaapp.com/index.php/Rest/WPConfig'
-qstring='u=jiangzemin&p=jzmjzmjzm&v=2.2.0&t=wp&l=wpl%d'
+start='http://newcdb.sinaapp.com/index.php/Client/V%s/Connect/?%s'%(cproxyver.replace('.',''),rrange(100,999))
+#targ='http://newcdb.sinaapp.com/index.php/Rest/WPConfig'
+qstring='u=jiangzemin&p=jzmjzmjzm&v=%s&t=wp&l=wpl%d'
 #http://mycdb.sinaapp.com/index.php/Rest/WPTrafficRevc?&tguid=423b7d8881f061c56b26308e1d744d94&ll=1048757
 ht=httplib2.Http()
-def get_tguid(wpid):
-	resp,ct=ht.request(start,method='POST',headers=hdstart,body=qstring%wpid)
-	tguid=re.findall('<tguid>(.+)</tguid>',ct)[0]
-	print('using tguid: %s'%tguid)
-	return tguid
-i=10
+# def get_tguid(wpid):
+# 	resp,ct=ht.request(start,method='POST',headers=hdstart,body=qstring%(cproxyver,wpid))
+# 	tguid=re.findall('<tguid>(.+)</tguid>',ct)[0]
+# 	print('using tguid: %s'%tguid)
+# 	return tguid
+#i=10
 fname='appid'
 fdl='D:\\Dev\\Python\\Workspace\\httplib2-plus-exp\\'
 open('%scproxy_%s.txt'%(fdl,fname),'a')
 found_all=open('%scproxy_%s.txt'%(fdl,fname),'r').read().split('|')
 found_cnt=len(found_all)
+
+wpid=1
 while(True):
-	if i>=10:
-		tguid=get_tguid(1)
-		i=0
-	resp,ct=ht.request(targ,method='POST',headers=hdtarg,body='tguid=%s'%tguid)
-	print ct
+	# if i>=10:
+	# 	tguid=get_tguid(0)
+	# 	i=0
+	resp,ct=ht.request(start,method='POST',headers=hdtarg,body=qstring%(cproxyver,wpid))
+	#print ct
 	section=re.findall('\[gae\](.*?)\[',ct,re.DOTALL)[0]
 	#found=re.findall('fetchserver.*?=.*?(.+)',section)[0].strip(' ').split('|')
 	found=re.findall('appid.*?=.*?(.+)',section)[0].strip(' ').split('|')
@@ -44,4 +47,4 @@ while(True):
 	print('found %d new %d all: %s'%(len(found_all)-found_cnt,len(found_all),','.join(found)))
 	found_cnt=len(found_all)
 	open('%scproxy_%s.txt'%(fdl,fname),'w').write('|'.join(sorted(found_all)))
-	i+=1
+	#i+=1
